518d07623166aeb7a639946e5a6f904a50e8b1f2
akshit <akshit.kanani.contractor@eqonex.com>
2022-07-04 15:44:38 +0530

    name add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+ const [input , setInput] = useState()
+
+const addItem =(e)=>{
+  e.preventDefault();
+  console.log("Kishan Patel ");
+}
+
   return (
     <div className="App">
-      <h1>Nilesh Patel</h1>
-      <h1>kishan patel </h1>
-      <h1>Gadara jay</h1>
+      <h1>Pagination Page</h1>
+      <h2>Kishan Patel </h2>
+
+      <label>Name</label>
+      <input type="text" value={input} onChange={(e)=>setInput(e.target.value)} />
+      <br/>
+      <label></label>
+      <input type="text" />
+      <br/>
+      <input type="text" />
+      <br/>
+      <button type="button" onClick={(e)=>addItem(e)}>Submit</button>
+      {/* <label>1</label>
+      <label>2</label>
+      <label>3</label>
+      <label>4</label> */}
     </div>
   );
 }
